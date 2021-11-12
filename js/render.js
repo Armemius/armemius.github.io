25b1c20a73361aebffd106ec7bccb5be7244d1d4
RusgyCufu <rusgy@yandex.ru>
2021-11-12 21:18:26 +0300

    few graphics

--- js/render.js
+++ js/render.js
@@ -1,22 +1,44 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#background")
+});
 renderer.setSize(window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.SphereGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshStandardMaterial( { color: 0xff0000} );
 const cube = new THREE.Mesh( geometry, material );
 scene.add(cube);
 
 camera.position.z = 5;
 
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+scene.add(pointLight);
+
+const ambientLight = new THREE.AmbientLight(0x404040);
+scene.add(ambientLight);
+
+var trueShift = 0, shift = 0;
+
 const animate = function() {
+    shift = (trueShift + shift * 9) / 10;
+    console.info(shift + " " + trueShift);
+    cube.rotation.x = shift * 0.01;
+    cube.rotation.y = shift * 0.01;
 	requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
 };
 
+function scroll() {
+    trueShift = -document.body.getBoundingClientRect().top;
+}
+function resize() {
+    camera.setAspect(window.innerWidth / window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
 animate();
+document.body.onscroll = scroll;
+document.body.onresize = resize;
