80054b82432eaeba559938c16b4d06626872653e
RusgyCufu <rusgy@yandex.ru>
2021-11-12 23:01:55 +0300

    stars

--- js/render.js
+++ js/render.js
@@ -1,5 +1,5 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 100);
 
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector("#background")
@@ -7,9 +7,9 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight );
 
 const geometry = new THREE.DodecahedronGeometry();
-const material = new THREE.MeshStandardMaterial( { color: 0xff0000} );
-const cube = new THREE.Mesh( geometry, material );
-scene.add(cube);
+const green = new THREE.MeshStandardMaterial( { color: 0x26FF7D} );
+const orange = new THREE.MeshStandardMaterial( { color: 0xFF9F00} );
+const violet = new THREE.MeshStandardMaterial( { color: 0x960DFF} );
 
 camera.position.z = 5;
 
@@ -22,14 +22,30 @@ scene.add(ambientLight);
 
 var trueShift = 0, shift = 0;
 
+for (var i = 0; i < 300; ++i) {
+    for (var j = 0; j < 10; ++j) {
+        const geometry = new THREE.SphereGeometry();
+        const material = new THREE.MeshStandardMaterial( { color: 0xCCCCFF} );
+        const star = new THREE.Mesh( geometry, material );
+        star.scale.set(0.15, 0.15, 0.15)
+        star.position.z = -i;
+        
+        star.position.x = Math.random() * 300 - 150;
+        star.position.y = Math.random() * 300 - 150;
+        scene.add(star);
+    }
+}
+
 const animate = function() {
     shift = (trueShift + shift * 9) / 10;
     console.info(shift + " " + trueShift);
-    cube.rotation.x = shift * 0.01;
-    cube.rotation.y = shift * 0.01;
+
+    camera.position.z = shift * -0.06 + 5;
+
 	requestAnimationFrame(animate);
     renderer.render(scene, camera);
 };
+animate();
 
 function scroll() {
     trueShift = -document.body.getBoundingClientRect().top;
@@ -39,6 +55,5 @@ function resize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-animate();
 document.body.onscroll = scroll;
 document.body.onresize = resize;
