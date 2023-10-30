e7ab973c5a2e6e66c8b38394cbc6061e0cdc3728
Armemius <rusgy@yandex.ru>
2023-10-31 01:07:25 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './style/App.css'
+
+const App = () => {
+    return (
+        <>
+            <div className="static-fullscreen screen-door-filter"></div>
+            <div className="static-fullscreen flicker"></div>
+            <div className="cross-decoration left top"></div>
+            <div className="cross-decoration left bottom"></div>
+            <div className="cross-decoration right top"></div>
+            <div className="cross-decoration right bottom"></div>
+            <div className="static-fullscreen blur"></div>
+            <div className="header-container">
+                <h1>[ IN DEVELOPMENT ]</h1>
+            </div>
+        </>
+    )
+}
+
+export default App
