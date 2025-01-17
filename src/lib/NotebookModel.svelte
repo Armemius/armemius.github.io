c3742779cd1e2d00b2c32c2f63bde9fd8595e4d6
Armemius <rusgy@yandex.ru>
2025-01-17 12:06:09 +0300

    [+] Notebook model renderer

---
+++ src/lib/NotebookModel.svelte
@@ -0,0 +1,99 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import * as THREE from "three";
+  import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+  import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+  let canvas: HTMLCanvasElement;
+
+  const UPSCALE_FACTOR = 4;
+
+  onMount(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      60,
+      canvas.width / canvas.height,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true,
+      alpha: true,
+    });
+
+    renderer.setSize(canvas.width, canvas.height);
+    document.body.appendChild(renderer.domElement);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+    scene.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(5, 10, 7.5);
+    scene.add(directionalLight);
+
+    const loader = new GLTFLoader();
+    loader.load(
+      "/laptop.glb",
+      (gltf) => {
+        scene.add(gltf.scene);
+        gltf.scene.scale.set(1, 1, 1);
+        gltf.scene.position.set(0, 0, 0);
+      },
+      undefined,
+      (error) => {
+        console.error("Error loading GLB model:", error);
+      }
+    );
+
+    camera.position.set(1.28, 4.61, 3.62);
+    camera.rotation.set(-0.41, 0.27, 0.12);
+    camera.zoom = 1;
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    const multiplier = 0.1;
+    controls.target.set(0.3, 3.3, 0.5);
+    controls.update();
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.001;
+    controls.enableZoom = false;
+
+    const initialAzimuthalAngle = controls.getAzimuthalAngle();
+    const initialPolarAngle = controls.getPolarAngle();
+
+    const limitAngle = THREE.MathUtils.degToRad(15);
+    controls.minAzimuthAngle = initialAzimuthalAngle - limitAngle;
+    controls.maxAzimuthAngle = initialAzimuthalAngle + limitAngle;
+    controls.minPolarAngle = initialPolarAngle - limitAngle;
+    controls.maxPolarAngle = initialPolarAngle + limitAngle;
+
+    const animate = () => {
+      controls.update();
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    window.addEventListener("resize", () => {
+      camera.aspect = canvas.width / canvas.height;
+      camera.updateProjectionMatrix();
+      renderer.setSize(
+        canvas.width * UPSCALE_FACTOR,
+        canvas.height * UPSCALE_FACTOR
+      );
+    });
+
+    return () => {
+      window.removeEventListener("resize", () => {});
+    };
+  });
+</script>
+
+<canvas width="600" height="500" bind:this={canvas}></canvas>
+
+<style>
+  canvas {
+    display: block;
+  }
+</style>
