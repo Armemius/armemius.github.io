074e6828cf5f5bd33430abbece075a1307ae2774
Armemius <rusgy@yandex.ru>
2025-01-17 09:39:04 +0300

    [+] Configure UnoCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import UnoCSS from 'unocss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), UnoCSS()],
   base: "/",
 });
