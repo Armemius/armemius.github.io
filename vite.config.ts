a2938d4ae8dd3a76867d34b0dcd84c6acbd05a3f
Armemius <rusgy@yandex.ru>
2025-01-17 09:40:24 +0300

    [+] Add export aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import UnoCSS from 'unocss/vite'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), UnoCSS()],
   base: "/",
+  resolve: {
+    alias: {
+      $lib: path.resolve('./src/lib'),
+      $assets: path.resolve('./src/assets')
+    },
+  },
 });
