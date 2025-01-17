a0f949e16fc47f9a952b87dd7abfeacdbfcaf13c
Armemius <rusgy@yandex.ru>
2025-01-17 13:58:02 +0300

    [+] Add Header component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import NotebookModel from "$lib/NotebookModel.svelte";
+  import BriefInfo from "$lib/BriefInfo.svelte";
+</script>
+
+<section class="grid grid-cols-1 md:grid-cols-2 h-screen items-center justify-items-center pl-[10%] pr-10%">
+  <BriefInfo />
+  <NotebookModel />
+</section>
