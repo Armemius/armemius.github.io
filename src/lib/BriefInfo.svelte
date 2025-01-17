67567a81d852285c9b9ac5c5c5f226714fc0144d
Armemius <rusgy@yandex.ru>
2025-01-17 13:57:23 +0300

    [+] Add brief info section

---
+++ src/lib/BriefInfo.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Button from "@smui/button";
+</script>
+
+<article class="relative mb-50 mr-20 flex flex-col gap-4">
+  <h1 class="text-10 font-500">Привет!</h1>
+  <p class="leading-9 text-6 font-350">
+    Меня зовут <span class="color-#6200ee">Арсений</span>, я люблю программирование, компьютеры и всё, что с ними
+    связано
+  </p>
+  <div>
+    <Button outlined>Обо мне</Button>
+    <Button outlined>Мои проекты</Button>
+  </div>
+</article>
