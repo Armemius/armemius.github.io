48f6a1bf395681994d76830f86a0f52393c72fcd
Armemius <rusgy@yandex.ru>
2025-01-18 22:27:28 +0300

    [~] Update BriefInfo

--- src/lib/BriefInfo.svelte
+++ src/lib/BriefInfo.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <article class="relative mb-50 mr-20 flex flex-col gap-4">
+  <div class="absolute top-[-15px] left-[-25px] bottom-[-15px] bg-black w-[4px] rounded-[2px]"></div>
   <h1 class="text-10 font-500">Привет!</h1>
   <p class="leading-9 text-6 font-350">
     Меня зовут <span class="color-#6200ee">Арсений</span>, я люблю программирование, компьютеры и всё, что с ними
