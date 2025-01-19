f472095d88b85320c2321b2064f3d39af6649cdf
Armemius <rusgy@yandex.ru>
2025-01-19 23:57:50 +0300

    [+] Projects section

---
+++ src/lib/ProjectItem.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import Tooltip from "sv-tooltip";
+  import type { Language } from "$lib/data/projects";
+
+  export let title: string;
+  export let description: string;
+  export let image: string;
+  export let github: string;
+  export let languages: Language[];
+
+  const languageIcons = {
+    any: "hidden",
+    c: "devicon-c-plain",
+    cpp: "devicon-cplusplus-plain",
+    rust: "devicon-rust-plain",
+    asm: "devicon-assembly-plain",
+    python: "devicon-python-plain",
+    js: "devicon-javascript-plain",
+    ts: "devicon-typescript-plain",
+    java: "devicon-java-plain",
+    kotlin: "devicon-kotlin-plain",
+    matlab: "devicon-matlab-plain",
+  };
+
+  const languageNames = {
+    any: "Не указан",
+    c: "C",
+    cpp: "C++",
+    rust: "Rust",
+    asm: "Ассемблер",
+    python: "Python",
+    js: "JavaScript",
+    ts: "TypeScript",
+    java: "Java",
+    kotlin: "Kotlin",
+    matlab: "MATLAB",
+  };
+</script>
+
+<section class="p-5 bg-gray-100 rounded-md">
+  <div class="flex flex-col md:flex-row gap-6">
+    <div class="w-full md:w-1/3">
+      <img src={image} alt={title} class="w-full h-auto" />
+    </div>
+    <div class="w-full md:w-2/3">
+      <h2 class="text-5 md:text-6 leading-8 font-500 mb-3">{title}</h2>
+      <p class="leading-6 text-4 md:leading-7 md:text-4 font-350">
+        {description}
+      </p>
+      <div class="flex gap-1 mt-3">
+        {#each languages as language}
+          <Tooltip
+            tip={languageNames[language]}
+            bottom>
+            <i class={`${languageIcons[language]} text-5`}></i>
+          </Tooltip>
+        {/each}
+      </div>
+      <a
+        href={github}
+        target="_blank"
+        class="text-4 md:text-5 font-350 color-black underline-none mt-3 block ">GitHub</a
+      >
+    </div>
+  </div>
+</section>
--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -1,5 +1,64 @@
 <script lang="ts">
+  import TextField from "@smui/textfield";
+  import Select, { Option } from "@smui/select";
+  import ProjectItem from "./ProjectItem.svelte";
+  import projects, { type Target, type Language } from "$lib/data/projects";
+
+  let search = "";
+  let language: Language = "any";
+  let target: Target = "any";
+
   export let element: HTMLElement;
 </script>
 
-<section bind:this={element} class="min-h-[100vh]">Projects</section>
+<section
+  bind:this={element}
+  class="min-h-[100vh] flex flex-col items-center p-15"
+>
+  <section class="center pb-10">
+    <h1 class="text-6 md:text-7 leading-9 font-500 mb-3 text-center">
+      Мои проекты
+    </h1>
+    <p class="leading-6 text-4 md:leading-7 md:text-5 font-350">
+      Здесь представлены мои проекты, над которыми я работал в свободное время
+    </p>
+  </section>
+  <div class="flex flex-col md:flex-row gap-6 mb-8">
+    <TextField bind:value={search} class="w-80" label="Поиск по названию" />
+    <Select bind:value={language} label="Язык">
+      <Option value="any" default>Любой</Option>
+      <Option value="c">C</Option>
+      <Option value="cpp">C++</Option>
+      <Option value="rust">Rust</Option>
+      <Option value="asm">Ассемблер</Option>
+      <Option value="python">Python</Option>
+      <Option value="js">JavaScript</Option>
+      <Option value="ts">TypeScript</Option>
+      <Option value="java">Java</Option>
+      <Option value="kotlin">Kotlin</Option>
+      <Option value="matlab">MATLAB</Option>
+    </Select>
+    <Select bind:value={target} label="Тип">
+      <Option value="any" default>Любой</Option>
+      <Option value="lib">Библиотека</Option>
+      <Option value="cli">Консольное приложение</Option>
+      <Option value="web">Веб-приложение</Option>
+      <Option value="desktop">Десктопное приложение</Option>
+      <Option value="embedded">Встроенные системы</Option>
+    </Select>
+  </div>
+  <div class="flex flex-wrap gap-7 justify-center">
+    {#each projects
+      .filter((it) => search === "" || it.title.includes(search))
+      .filter((it) => language === "any" || it.languages.includes(language))
+      .filter((it) => target === "any" || it.target === target) as project}
+      <ProjectItem {...project} />
+    {/each}
+    {#if !projects
+      .filter((it) => search === "" || it.title.includes(search))
+      .filter((it) => language === "any" || it.languages.includes(language))
+      .filter((it) => target === "any" || it.target === target).length}
+      <p class="text-4 font-350">Ничего не найдено :(</p>
+    {/if}
+  </div>
+</section>
