0ae22d81852fac1b008ec874a246bb1e8f7514ee
Armemius <rusgy@yandex.ru>
2025-01-19 16:58:11 +0300

    [+] Add scrolling behaviour

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
   import Header from "$lib/Header.svelte";
+  import About from "$lib/About.svelte";
+  import Projects from "$lib/Projects.svelte";
+
+  let headerSection: HTMLElement;
+  let aboutSection: HTMLElement;
+  let projectsSection: HTMLElement;
 </script>
 
 <main>
-  <Header />
+  <Header bind:element={headerSection} {aboutSection} {projectsSection} />
+  <About bind:element={aboutSection} />
+  <Projects bind:element={projectsSection} />
 </main>
--- src/lib/BriefInfo.svelte
+++ src/lib/BriefInfo.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
   import Button from "@smui/button";
   import { fadeIn } from "$lib/animation/fadeIn";
+
+  export let aboutSection: HTMLElement;
+  export let projectsSection: HTMLElement;
+
+  const scrollToSection = (section: HTMLElement) => {
+    return () => {
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    };
+  };
 </script>
 
 <article use:fadeIn class="relative mb-50 mr-20 flex flex-col gap-4">
@@ -10,7 +22,11 @@
     программирование, компьютеры и всё, что с ними связано
   </p>
   <div>
-    <Button>Обо мне</Button>
-    <Button>Мои проекты</Button>
+    <button on:click={scrollToSection(aboutSection)}>
+      <Button>Обо мне</Button>
+    </button>
+    <button on:click={scrollToSection(projectsSection)}>
+      <Button>Мои проекты</Button>
+    </button>
   </div>
 </article>
